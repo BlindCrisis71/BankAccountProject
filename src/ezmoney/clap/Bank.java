ac207a4bf9051578affe28974719f64842bff6c4
Brandyn Call <blind_crisis71@hotmail.com>
2019-07-09 21:42:52 -0600

    Initial commit

---
+++ src/ezmoney/clap/Bank.java
@@ -0,0 +1,12 @@
+package ezmoney.clap;
+
+public class Bank {
+
+    // Very important variable
+    private final boolean isMuchiWallaceABitch = true;
+
+    // Contains very important information the world must know
+    public boolean isMuchiWallaceABitch() {
+        return isMuchiWallaceABitch;
+    }
+}
