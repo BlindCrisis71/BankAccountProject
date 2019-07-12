17deaa8eecae97884761c06f9d7cc1a8ac4ad9e9
mushy <mujtabaashfaq@yahoo.com>
2019-07-11 19:44:12 -0600

    Added default classes for the project.

--- src/ezmoney/clap/Bank.java
+++ src/ezmoney/clap/Bank.java
@@ -2,11 +2,74 @@ package ezmoney.clap;
 
 public class Bank {
 
-    // Very important variable
-    private final boolean isMuchiWallaceABitch = true;
+    public static void main(String[] args) {
 
-    // Contains very important information the world must know
-    public boolean isMuchiWallaceABitch() {
-        return isMuchiWallaceABitch;
+        //This is the driver class
+
+
+        boolean login = false;
+
+
+/*
+        //Loop through login attemps
+        while(!login){
+
+            print
+            //Get credentials
+            cout << Enter name
+            cout << Enter pasword
+
+            //See if account exists
+            if(name){
+
+                if(password){
+
+                    login = true;
+
+                    tempaccount = list.get(name, pin)
+
+                }else{
+
+                    cout << "wrong pin"
+                }
+
+                cout << "wrong username"
+            }
+        }
+
+
+
+        //Loop through options for this account
+        if(manager){
+
+            while(!exit){
+
+                //Get input from user
+                cout << "Selection"
+                cout << "Enter selection number"
+
+
+                //Do something with input
+
+                switch(input){
+
+                    case 1:
+                        cout << "Enter name for new acount"
+                        cout << "Enter account number"
+
+                        manager.createNewAccount(name, accountnumber);
+
+                        break;
+                    case 2:
+                        manager.delete(list)
+                        break;
+                    etc;
+                }
+
+            }
+        }
+*/
+
     }
+
 }
